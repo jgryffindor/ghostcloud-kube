fed5b914429d2b91016fda2ee2a01dfc5fadcb2f
Justin Griffin <jgriffin555@comcast.net>
2023-10-05 16:10:36 -0500

    removed: Removing @dot-i/k8s-operator package, implementing our own resource monitoring within scheduler service

--- src/main.ts
+++ src/main.ts
@@ -3,32 +3,6 @@ import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { AppConfigService } from "./config/app/configuration.service";
-import Operator, {
-  ResourceEventType,
-  ResourceEvent,
-} from "@dot-i/k8s-operator";
-
-import { Base, Events, Network, Web, Ledger } from "@liftedinit/many-js";
-
-export default class GhostcloudOperator extends Operator {
-  protected async init() {
-    await this.watchResource("", "v1", "namespaces", async (e) => {
-      const object = e.object;
-      const metadata = object.metadata;
-      switch (e.type) {
-        case ResourceEventType.Added:
-          // do something useful here
-          break;
-        case ResourceEventType.Modified:
-          // do something useful here
-          break;
-        case ResourceEventType.Deleted:
-          // do something useful here
-          break;
-      }
-    });
-  }
-}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -52,7 +26,6 @@ async function bootstrap() {
   SwaggerModule.setup("api", app, document);
 
   await app.listen(appConfig.port);
-
 }
 
 bootstrap();
